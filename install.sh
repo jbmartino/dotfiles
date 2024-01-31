793b91f02fcb4faf8baf864d44c92496a39b967c
jmartin <jmartin@eezy.com>
2024-01-30 23:55:33 -0500

    init install.sh

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Check for Oh My Zsh and install if we don't have it
+if test ! $(which omz); then
+  /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/HEAD/tools/install.sh)"
+fi
+
+# Check for Homebrew and install if we don't have it
+if test ! $(which brew); then
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+brew update
+
+# Install dependencies with bundle (Brewfile)
+brew tap homebrew/bundle
+brew bundle
